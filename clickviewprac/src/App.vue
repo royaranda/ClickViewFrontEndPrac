294162203278215a44e843b5e34eda7a5353583b
royaranda <royaranda2@gmail.com>
2019-01-11 11:21:56 +1100

    Set up Vue project files, moved video.js to /src folder

---
+++ clickviewprac/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Videos />
+  </div>
+</template>
+
+<script>
+import Videos from './components/Videos.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Videos
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
